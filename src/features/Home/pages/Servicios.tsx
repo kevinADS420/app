1dd9f7b22f62595374c4063c7197a1384c368597
Luisa <luisafernandamajin0222@gmail.com>
2025-03-05 21:01:41 -0500

    inicio texto

--- src/features/Home/pages/Servicios.tsx
+++ src/features/Home/pages/Servicios.tsx
@@ -1,13 +1,23 @@
+import '../../../style/Servicios.css'
 
-
-function SobreNosotros() {
+function Servicios() {
   return (
+    
+    
     <>  
-        <h1>Hola mundo </h1>
-        <p>EN el mundo hay gente</p>
+          <img className="imagen" src="src/assets/images/servicios.jpg" alt="" />
 
+        <h1 className="texto">SERVICIOS BRINDADOS  </h1>
+        <p>Mercado Huerto: El Puente Entre el Campo y la Ciudad
+En la vida cotidiana, los campesinos desempeñan un papel fundamental que a menudo pasa desapercibido. Su trabajo incansable nos conecta con la tierra y nos proporciona los alimentos que nutren nuestros cuerpos.
+Huerto Market nace como un proyecto que honra esta conexión vital, creando un espacio donde las frutas y verduras cultivadas con dedicación llegan directamente del campo a la mesa. Este mercado no es solo un lugar de comercio , sino un punto de encuentro entre dos mundos: el rural y el urbano.
+Los agricultores que colaboran con Huerto Market cultivan sus productos respetando los ciclos naturales y aplicando técnicas sostenibles que preservan la biodiversidad y la salud del suelo . Estas prácticas garantizan alimentos frescos de alta calidad nutricional .
+Cada producto que llega a Huerto Market cuenta una historia de trabajo , tradición y conocimiento ancestral . Las familias campesinas transmiten de generación en generación sus saberes sobre el cultivo de la tierra, manteniendo vivo un patrimonio cultural invaluable.
+Al elegir Huerto Market, los consumidores participan en un comercio justo que valora adecuadamente el esfuerzo del campesino y promueve una economía local más equitativa. Este vínculo directo elimina intermediarios innecesarios y fortalece la comunidad .
+Huerto Market, donde la frescura de la huerta llega a la cotidianidad urbana, transformando cada comida en una celebración de nuestra conexión con la naturaleza y con quienes la cultivan.</p>
+      
     </>
   );
 }
 
-export default SobreNosotros;
+export default Servicios;
