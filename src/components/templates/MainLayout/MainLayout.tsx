50671aeafa5cf4e48333542175ad1ccb5969eab4
kevinADS420 <kevin.aguirre.ads@gmail.com>
2025-03-24 08:43:40 -0500

    Actualisacion del front

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../organisms/Header';
+import Header from '../../organisms/header';
 import Footer from '../../organisms/Footer';
 import './MainLayout.css';
 
